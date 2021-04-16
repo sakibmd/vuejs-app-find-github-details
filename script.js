00e41f238fc5710570cfc7de3f2b2be2c6c76465
Sakib Mohammed <sakibmd.cse@gmail.com>
2021-04-16 16:23:45 +0600

    form ready, show details using username

---
+++ script.js
@@ -0,0 +1,28 @@
+const GitInfo = {
+    data() {
+        return {
+            username: "",
+            userinfo: [],
+            showresult: false,
+        };
+    },
+    methods: {
+        get_details() {
+            fetch("https://api.github.com/users/" + this.username)
+                .then((res) => {
+                    return res.json();
+                })
+                .then((data) => {
+                    this.userinfo = data;
+                    console.log(this.userinfo.company);
+                });
+            this.showresult = true;
+        },
+        reset() {
+            this.username = "";
+            this.showresult = false;
+            this.userinfo = [];
+        },
+    },
+}
+Vue.createApp(GitInfo).mount('#git_info')
